d219f949c5bc30c46b28d01e920ebeed7f968011
Pablo Godoy <pablo.godoy@bairesdev.com>
2024-02-22 21:02:39 -0600

    fix app.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Invitation from './components/Invitation/Invitation';
 
-const router = createBrowserRouter([
-  {
-    path:'/',
-    Component: Invitation
-  }
-]);
 
 const App: React.FC = () => {
-  return <RouterProvider router={router} />;
+  return <Invitation />;
 };
 
 export default App;
